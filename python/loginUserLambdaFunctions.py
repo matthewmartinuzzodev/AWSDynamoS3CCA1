b63ddf7fcfb3009c87e36fe7233d678f3baeb9f7
RMITmatthewmartinuzzo <matthewmartinuzzo.dev@gmail.com>
2024-04-14 17:07:09 +1000

    lambda login/register functions

---
+++ python/loginUserLambdaFunctions.py
@@ -0,0 +1,96 @@
+"""
+Code adapted from week 5 learning examples:
+https://rmit.instructure.com/courses/125083/pages/week-5-learning-materials-slash-activities?module_item_id=6033330
+"""
+
+import boto3
+from boto3.dynamodb.conditions import Key
+
+def lambda_handler(event, context):
+    type = event['type']
+    user = event['user']
+    userEmail = user['email']
+    userPassword = user['password']
+    client = boto3.resource('dynamodb')
+    table = client.Table('login')
+    if type == "validate":
+        # correctPassword = table.get_item(
+        #     Key={
+        #         'email' : userEmail,
+        #         'user_name' : 'Matthew Martinuzzo1'
+        #     }
+        # )['Item']['password']
+        # if (correctPassword == userPassword):
+        #     resp = True;
+        tableQuery = table.query(
+            KeyConditionExpression=Key('email').eq(userEmail)
+        )
+
+        if (tableQuery['Count'] == 1 and tableQuery['Items'][0]['password'] == userPassword):
+            resp = {
+                'statusCode': 200,
+                'headers': {
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': '*'
+                },
+                'body': {
+                    'Valid' : True,
+                }
+            }
+        else:
+            resp = {
+                'statusCode': 200,
+                'headers': {
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': '*'
+                },
+                'body': {
+                    'Valid' : False,
+                }
+            }
+        
+    elif type == "register":
+        userName = user['user_name']
+        try:
+            tableQuery = table.get_item(
+                Key = {
+                    'email' : userEmail,
+                    'user_name' : userName
+                }
+            )['Item']
+            
+            resp = {
+                'statusCode': 200,
+                'headers': {
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': '*'
+                },
+                'body': {
+                    'Response' : 'Email already exists in database',
+                }
+            }
+        except:
+            table.put_item(
+                Item = {
+                    'email' : userEmail,
+                    'user_name' : userName,
+                    'password' : userPassword
+                }
+            )
+            resp = {
+                'statusCode': 200,
+                'headers': {
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': '*'
+                },
+                'body': {
+                    'Response' : 'User successfully added to database',
+                }
+            }
+        
+    return resp
+        
